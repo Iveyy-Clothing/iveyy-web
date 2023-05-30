246a6d8a951c2713460093a0d66bc6ddb88e267b
elivnatwiine@gmail.com <elivnatwiine@gmail.com>
2023-05-29 23:03:26 -0700

    nav done

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -14,7 +14,7 @@ export default function Navigation() {
 
   return (
     <header className="absolute inset-x-0 top-0 z-50">
-      <nav className="bg-opacity-0 border-b border-white max-w-7xl mx-auto px-2 sm:px-6 lg:px-8" aria-label="Global">
+      <nav className="bg-opacity-0 max-w-7xl mx-auto px-2 sm:px-6 lg:px-8" aria-label="Global">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0 flex items-center">
             <a href="/">
